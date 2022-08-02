ce377cc368c826f3c6286f8b35b09471e92e97f7
vanithavicky91 <109418297+vanithavicky91@users.noreply.github.com>
2022-08-02 11:06:35 +0530

    Add files via upload

---
+++ Fibbonocci.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Fibbonocci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int N=11, n1=0, n2=1;
+System.out.println("fib of "+N+" numbers");
+for (int i = 1; i <=N; ++i) {
+	System.out.println(n1+" ");
+	int sum=n1+n2;
+	n1=n2;
+	n2=sum;
+	
+	
+}
+	
+	
+}
+	}
+
+
